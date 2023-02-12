2aae8560f0a8edbc0dea4612bfb34d73c45f4017
Phathdt <phathdt379@gmail.com>
2023-02-12 16:40:15 +0700

    replace nextjs with vitejs

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="text-success text-center">Hello world</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
