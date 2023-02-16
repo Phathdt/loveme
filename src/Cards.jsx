dd368b9f70340e08f8b42ec9aa0f8c8e41958351
Phathdt <phathdt379@gmail.com>
2023-02-17 00:42:35 +0700

    gst

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -19,21 +19,24 @@ export default function Cards() {
   return (
     <div className="container">
       <div className="row row-cols-1 row-cols-md-3 g-4">
-        {records.slice(0, 9).map((record, index) => {
-          return (
-            <div className="col" key={index}>
-              <div className="card h-100">
-                <div
-                  className="card-body"
-                  style={{ border: '10px solid #cde0ef' }}
-                >
-                  <h5 className="card-title">{record.name}</h5>
-                  <p className="card-text">{record.your_wish}</p>
+        {records
+          .filter((t) => t?.show === 'x')
+          .slice(0, 9)
+          .map((record, index) => {
+            return (
+              <div className="col" key={index}>
+                <div className="card h-100">
+                  <div
+                    className="card-body"
+                    style={{ border: '10px solid #cde0ef' }}
+                  >
+                    <h5 className="card-title">{record.name}</h5>
+                    <p className="card-text">{record.your_wish}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          )
-        })}
+            )
+          })}
       </div>
     </div>
   )
