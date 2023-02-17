d54ab24edb23f45a74e23636b1326ce0ccd5e7d5
Phathdt <phathdt379@gmail.com>
2023-02-17 22:13:28 +0700

    update show card random

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 export default function Cards() {
   const [records, setRecords] = useState([])
+  const [record, setRecord] = useState(null)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,28 +21,39 @@ export default function Cards() {
     fetchData()
   }, [])
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const selectedRecords = records.filter((t) => t.show === 'x')
+      const max = selectedRecords.length
+
+      const randomNumber = randomIntFromInterval(0, max - 1)
+
+      setRecord(selectedRecords[randomNumber])
+    }, 2000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [records.length])
+
   return (
     <div className="container">
-      <div className="row row-cols-1 row-cols-md-3 g-4">
-        {records
-          .filter((t) => t?.show === 'x')
-          .slice(0, 9)
-          .map((record, index) => {
-            return (
-              <div className="col" key={index}>
-                <div className="card h-100">
-                  <div
-                    className="card-body"
-                    style={{ border: '10px solid #cde0ef' }}
-                  >
-                    <h5 className="card-title">{record.name}</h5>
-                    <p className="card-text">{record.your_wish}</p>
-                  </div>
-                </div>
+      {record && (
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          <div className="col" />
+          <div className="col">
+            <div className="card h-100">
+              <div
+                className="card-body"
+                style={{ border: '10px solid #cde0ef' }}
+              >
+                <h5 className="card-title">{record?.name}</h5>
+                <p className="card-text">{record?.your_wish}</p>
               </div>
-            )
-          })}
-      </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
